dcfad9d53bd7a29c0da08a2b297c26685bb5fe7b
xwolo <xwoloszyk@gmail.com>
2024-12-10 21:37:28 +0100

    Dodano HTML z OpenStreeMap

--- bsp/routes.py
+++ bsp/routes.py
@@ -4,5 +4,5 @@ main = Blueprint('main', __name__)
 
 @main.route('/')
 def home():
-    return "Witaj na mojej stronie Flask!"
-    # render_template('index.html')
+    #return "Witaj na mojej stronie Flask!"
+    return render_template('index.html')
