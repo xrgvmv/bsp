c8818baaec7b21f0eafe57b37f8e06788bac6a51
patrykso <patryksowinskitoczek@gmail.com>
2025-05-25 17:41:04 +0200

    files structure fix

--- bsp-backend/run.py
+++ bsp-backend/run.py
@@ -1,11 +1,3 @@
-#from bsp import create_app
-
-#app = create_app()
-
-#if __name__ == '__main__':
-    #app.run(app.run(debug=True, host='0.0.0.0', port=5000))
-
-import threading
 from bsp import create_app
 from bsp.packets_generator import packets_generator
 
